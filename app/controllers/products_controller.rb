002db2b8d7c175ff2281f270303866e0940b5aed
YanC Lin <yonalin1@gmail.com>
2018-09-29 16:44:19 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,20 @@
 class ProductsController < ApplicationController
+  before_action :set_params, only: [:data, :inventory, :description, :show]
+
   def index
     @products = Product.all
   end
 
+  def data
+    render json: ProductSerializer.serialize(@product)
+  end
+
   def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
+    render plain: @product.inventory > 0 ? true : false
   end
 
   def description
-    product = Product.find(params[:id])
-    render plain: product.description
+    render plain: @product.description
   end
 
   def new
@@ -22,7 +26,14 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+  end
+
   private
+
+  def set_params
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
